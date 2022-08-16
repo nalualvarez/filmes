40530b9e080abbd9856c44250dbe3e8986a79a20
F3866218 Guilherme Oliveira Loiola <guilherme.loiola@bb.com.br>
2022-08-16 13:18:11 -0300

    estrutura inicial

--- Main.java
+++ Main.java
@@ -1,3 +1,36 @@
+import java.nio.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        
+        Path movies1 = Paths.get("filmes/movies1.csv");  
+        Path movies2 = Paths.get("filmes/movies2.csv");  
+        Path movies3 = Paths.get("filmes/movies3.csv");  
+        List<String> arquivo1 = new ArrayList<String>();
+        List<String> arquivo2 = new ArrayList<String>();
+        List<String> arquivo3 = new ArrayList<String>();
+    
+        try {
+            arquivo1 = Files.readAllLines(movies1);
+            arquivo2 = Files.readAllLines(movies2);
+            arquivo3 = Files.readAllLines(movies3);
+            
+            
+        } catch (Exception e) {
+            System.err.println("Erro na leitura dos arquivos");
+        }finally{
+            
+          
+        }
+    }
+    
 
 }
---
+++ Movie.java
@@ -0,0 +1,86 @@
+public class Movie {
+    int Rank;
+    String Title;
+    String Genre;
+    String Description;
+    String Director;
+    String Actors;
+    int Year;
+    int Runtime;
+    float Rating;
+    int Votes;
+    float Revenue;
+    int Metascore;
+    public int getRank() {
+        return Rank;
+    }
+    public void setRank(int rank) {
+        Rank = rank;
+    }
+    public String getTitle() {
+        return Title;
+    }
+    public void setTitle(String title) {
+        Title = title;
+    }
+    public String getGenre() {
+        return Genre;
+    }
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+    public String getDescription() {
+        return Description;
+    }
+    public void setDescription(String description) {
+        Description = description;
+    }
+    public String getDirector() {
+        return Director;
+    }
+    public void setDirector(String director) {
+        Director = director;
+    }
+    public String getActors() {
+        return Actors;
+    }
+    public void setActors(String actors) {
+        Actors = actors;
+    }
+    public int getYear() {
+        return Year;
+    }
+    public void setYear(int year) {
+        Year = year;
+    }
+    public int getRuntime() {
+        return Runtime;
+    }
+    public void setRuntime(int runtime) {
+        Runtime = runtime;
+    }
+    public float getRating() {
+        return Rating;
+    }
+    public void setRating(float rating) {
+        Rating = rating;
+    }
+    public int getVotes() {
+        return Votes;
+    }
+    public void setVotes(int votes) {
+        Votes = votes;
+    }
+    public float getRevenue() {
+        return Revenue;
+    }
+    public void setRevenue(float revenue) {
+        Revenue = revenue;
+    }
+    public int getMetascore() {
+        return Metascore;
+    }
+    public void setMetascore(int metascore) {
+        Metascore = metascore;
+    }
+}
