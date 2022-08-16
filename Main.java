40f46a9c83f84e3e91117cadc4466d18d3503c69
F3866218 Guilherme Oliveira Loiola <guilherme.loiola@bb.com.br>
2022-08-16 13:36:53 -0300

    parse atributos

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
-import java.nio.*;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+
+    static  List<Movie> parseLinha(List<String> linhas){
+
+        List<Movie> arquivo = new ArrayList<Movie>();
+
+        List<String[]> fields = linhas.stream().skip(1).map(buffer -> {
+            String[] atributos = buffer.split(",");
+            return atributos;
+        }).collect(Collectors.toList());;
+
+
+        //atributos aqui
+        //TODO: inicializar o objeto  List<Movie> arquivo
+        for (String[] strings : fields) {
+            for (String strings2 : strings) {
+                System.out.println(strings2);
+            }
+        }
+
+        return arquivo;
+
+    }
+
+
     public static void main(String[] args) {
+
+        //roda o main pra ver o parse funcionando
         
         Path movies1 = Paths.get("filmes/movies1.csv");  
         Path movies2 = Paths.get("filmes/movies2.csv");  
@@ -26,10 +50,9 @@ public class Main {
             
         } catch (Exception e) {
             System.err.println("Erro na leitura dos arquivos");
-        }finally{
-            
-          
         }
+
+        parseLinha(arquivo1);
     }
     
 
